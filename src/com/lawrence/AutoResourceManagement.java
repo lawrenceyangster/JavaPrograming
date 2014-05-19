c2dd5a6357698201b2c0ac25c118067636bf6b3d
Lawrence Yang <lawrenceyangster@gmail.com>
2014-05-19 22:39:17 +0800

    Automatic resource management

--- src/com/lawrence/AutoResourceManagement.java
+++ src/com/lawrence/AutoResourceManagement.java
@@ -1,5 +1,28 @@
 package com.lawrence;
 
-public class AutoResourceManagement {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+public class AutoResourceManagement {
+	/**
+	 * Copy the content of the src file to target file
+	 * @param src
+	 * @param target
+	 */
+	public static void copy(String src, String target)
+	{
+		try(InputStream in = new FileInputStream(src); OutputStream ou = new FileOutputStream(target))
+		{
+			int b;
+			while((b=in.read())!=-1)
+				ou.write(b);
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
 }
